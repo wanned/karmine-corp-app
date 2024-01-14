865053cb451976a24381294c3119ff45d18146b8
thierrymgn <maignan.thierry@outlook.fr>
2024-01-15 00:03:26 +0100

    Add Results and Next Matches modal in home

---
+++ src/screens/home/modals/next-matches-modal.tsx
@@ -0,0 +1,65 @@
+import { NavigationContainerRef, useNavigation } from '@react-navigation/native';
+import React from 'react';
+import { ScrollView, TouchableOpacity, View } from 'react-native';
+import { Iconify } from 'react-native-iconify';
+
+import { MatchScore } from '~/shared/components/match/match-score';
+import { MatchTeam } from '~/shared/components/match/match-team';
+import { useNextMatches } from '~/shared/hooks/data/use-next-matches';
+import { useStyles } from '~/shared/hooks/use-styles';
+import { RootStackParamList } from '~/shared/navigation';
+import { createStylesheet } from '~/shared/styles/create-stylesheet';
+
+export const NextMatchesModal = React.memo(() => {
+  const styles = useStyles(getStyles);
+
+  const navigation = useNavigation<NavigationContainerRef<RootStackParamList>>();
+
+  const matchs = useNextMatches();
+
+  if (!matchs.length) {
+    return null;
+  }
+
+  return (
+    <ScrollView style={styles.container}>
+      <TouchableOpacity onPress={() => navigation.goBack()} style={styles.iconContainer}>
+        <Iconify icon="solar:arrow-left-linear" size={28} color={styles.icon.color} />
+      </TouchableOpacity>
+      <View style={styles.matchesContainer}>
+        {matchs.map(
+          ({ data: match }, index) =>
+            match && (
+              <MatchScore
+                key={index}
+                date={match.date}
+                status="upcoming"
+                bo={'bo' in match.matchDetails ? match.matchDetails.bo : undefined}
+                game={match.matchDetails.game}>
+                {match.teams.map((team, index) => (
+                  <MatchTeam key={index} logo={team.logoUrl} name={team.name} score="-" />
+                ))}
+              </MatchScore>
+            )
+        )}
+      </View>
+    </ScrollView>
+  );
+});
+
+const getStyles = createStylesheet((theme) => ({
+  container: {
+    backgroundColor: theme.colors.background,
+  },
+  matchesContainer: {
+    marginTop: 8,
+    paddingHorizontal: 16,
+    paddingBottom: 16,
+  },
+  iconContainer: {
+    padding: 20,
+  },
+  icon: {
+    color: theme.colors.foreground,
+  },
+}));
