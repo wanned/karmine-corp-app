c9eefcbab075a7cd5d193a894b88e8d3dbd86d14
thierrymgn <maignan.thierry@outlook.fr>
2024-01-15 15:27:53 +0100

    Update main file to src/App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,13 @@
 import 'react-native-gesture-handler';
 
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { registerRootComponent } from 'expo';
 import { useFonts } from 'expo-font';
 import * as SplashScreen from 'expo-splash-screen';
 import { useCallback } from 'react';
 import { View } from 'react-native';
 
-import RootStack from './src/shared/navigation';
+import RootStack from './shared/navigation';
 
 import { SettingsProvider } from '~/shared/contexts/settings-context';
 import { ThemeContext } from '~/shared/contexts/theme-context';
@@ -19,19 +20,19 @@ const queryClient = new QueryClient();
 export default function App() {
   const [fontsLoaded] = useFonts({
     // Cairo
-    'Cairo-Black': require('./assets/fonts/Cairo/Cairo-Black.ttf'),
-    'Cairo-Bold': require('./assets/fonts/Cairo/Cairo-Bold.ttf'),
-    'Cairo-ExtraBold': require('./assets/fonts/Cairo/Cairo-ExtraBold.ttf'),
-    'Cairo-Medium': require('./assets/fonts/Cairo/Cairo-Medium.ttf'),
-    'Cairo-Regular': require('./assets/fonts/Cairo/Cairo-Regular.ttf'),
-    'Cairo-SemiBold': require('./assets/fonts/Cairo/Cairo-SemiBold.ttf'),
+    'Cairo-Black': require('../assets/fonts/Cairo/Cairo-Black.ttf'),
+    'Cairo-Bold': require('../assets/fonts/Cairo/Cairo-Bold.ttf'),
+    'Cairo-ExtraBold': require('../assets/fonts/Cairo/Cairo-ExtraBold.ttf'),
+    'Cairo-Medium': require('../assets/fonts/Cairo/Cairo-Medium.ttf'),
+    'Cairo-Regular': require('../assets/fonts/Cairo/Cairo-Regular.ttf'),
+    'Cairo-SemiBold': require('../assets/fonts/Cairo/Cairo-SemiBold.ttf'),
     // MonaspaceNeon
-    'MonaspaceNeon-Bold': require('./assets/fonts/MonaspaceNeon/MonaspaceNeon-Bold.otf'),
-    'MonaspaceNeon-ExtraLight': require('./assets/fonts/MonaspaceNeon/MonaspaceNeon-ExtraLight.otf'),
-    'MonaspaceNeon-Light': require('./assets/fonts/MonaspaceNeon/MonaspaceNeon-Light.otf'),
-    'MonaspaceNeon-Medium': require('./assets/fonts/MonaspaceNeon/MonaspaceNeon-Medium.otf'),
-    'MonaspaceNeon-Regular': require('./assets/fonts/MonaspaceNeon/MonaspaceNeon-Regular.otf'),
-    'MonaspaceNeon-SemiBold': require('./assets/fonts/MonaspaceNeon/MonaspaceNeon-SemiBold.otf'),
+    'MonaspaceNeon-Bold': require('../assets/fonts/MonaspaceNeon/MonaspaceNeon-Bold.otf'),
+    'MonaspaceNeon-ExtraLight': require('../assets/fonts/MonaspaceNeon/MonaspaceNeon-ExtraLight.otf'),
+    'MonaspaceNeon-Light': require('../assets/fonts/MonaspaceNeon/MonaspaceNeon-Light.otf'),
+    'MonaspaceNeon-Medium': require('../assets/fonts/MonaspaceNeon/MonaspaceNeon-Medium.otf'),
+    'MonaspaceNeon-Regular': require('../assets/fonts/MonaspaceNeon/MonaspaceNeon-Regular.otf'),
+    'MonaspaceNeon-SemiBold': require('../assets/fonts/MonaspaceNeon/MonaspaceNeon-SemiBold.otf'),
   });
 
   const onLayoutRootView = useCallback(async () => {
@@ -66,3 +67,5 @@ const _App = ({ onLayoutRootView }: { onLayoutRootView: () => void }) => {
     </View>
   );
 };
+
+registerRootComponent(App);
