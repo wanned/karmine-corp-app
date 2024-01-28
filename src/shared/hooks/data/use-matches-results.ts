97eed8364728b4d217d3b90d2be48d6650c58b2e
Arthur Fontaine <0arthur.fontaine@gmail.com>
2024-01-28 03:24:28 +0100

    Update next matches and matches results with the onResult dataFetcher event

--- src/shared/hooks/data/use-matches-results.ts
+++ src/shared/hooks/data/use-matches-results.ts
@@ -1,17 +1,34 @@
-import { useQuery } from '@tanstack/react-query';
+import { useQuery, useQueryClient } from '@tanstack/react-query';
+import { useEffect } from 'react';
 
 import { useDataFetcher } from './use-data-fetcher';
 
+import { CoreData } from '~/shared/data/core/types';
+
 export const useMatchesResults = () => {
   const dataFetcher = useDataFetcher();
+  const queryClient = useQueryClient();
 
-  const results = useQuery({
+  useEffect(() => {
+    dataFetcher.getSchedule({
+      filters: { status: ['finished', 'live'] },
+      onResult: (newMatch) => {
+        queryClient.setQueryData<CoreData.Match[]>(['matchesResults'], (matches = []) => {
+          const matchIndex = matches?.findIndex((match) => match.id === newMatch.id);
+
+          if (matchIndex === -1) {
+            return [...matches, newMatch];
+          }
+
+          return [...matches.slice(0, matchIndex), newMatch, ...matches.slice(matchIndex + 1)];
+        });
+      },
+    });
+  }, [queryClient, dataFetcher]);
+
+  const results = useQuery<CoreData.Match[]>({
     queryKey: ['matchesResults'],
-    queryFn: () =>
-      dataFetcher
-        .getSchedule({ filters: { status: ['finished', 'live'] } })
-        .then((matches) => matches.sort((a, b) => b.date.getTime() - a.date.getTime())),
-    throwOnError: true,
+    select: (matches) => matches.sort((a, b) => b.date.getTime() - a.date.getTime()),
   });
 
   return results;
--- src/shared/hooks/data/use-next-matches.ts
+++ src/shared/hooks/data/use-next-matches.ts
@@ -1,17 +1,34 @@
-import { useQuery } from '@tanstack/react-query';
+import { useQuery, useQueryClient } from '@tanstack/react-query';
+import { useEffect } from 'react';
 
 import { useDataFetcher } from './use-data-fetcher';
 
+import { CoreData } from '~/shared/data/core/types';
+
 export const useNextMatches = () => {
   const dataFetcher = useDataFetcher();
+  const queryClient = useQueryClient();
 
-  const results = useQuery({
+  useEffect(() => {
+    dataFetcher.getSchedule({
+      filters: { status: ['upcoming'] },
+      onResult: (newMatch) => {
+        queryClient.setQueryData<CoreData.Match[]>(['nextMatches'], (matches = []) => {
+          const matchIndex = matches?.findIndex((match) => match.id === newMatch.id);
+
+          if (matchIndex === -1) {
+            return [...matches, newMatch];
+          }
+
+          return [...matches.slice(0, matchIndex), newMatch, ...matches.slice(matchIndex + 1)];
+        });
+      },
+    });
+  }, [queryClient, dataFetcher]);
+
+  const results = useQuery<CoreData.Match[]>({
     queryKey: ['nextMatches'],
-    queryFn: () =>
-      dataFetcher
-        .getSchedule({ filters: { status: ['upcoming'] } })
-        .then((matches) => matches.sort((a, b) => a.date.getTime() - b.date.getTime())),
-    throwOnError: true,
+    select: (matches) => matches.sort((a, b) => a.date.getTime() - b.date.getTime()),
   });
 
   return results;
