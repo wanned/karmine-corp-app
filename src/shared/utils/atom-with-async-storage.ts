dd5b7e61732ac773eb109401248ebc70db03e987
Arthur Fontaine <0arthur.fontaine@gmail.com>
2024-02-26 11:03:25 +0100

    Optimize AsyncStorage update in atomWithAsyncStorage

--- src/shared/utils/atom-with-async-storage.ts
+++ src/shared/utils/atom-with-async-storage.ts
@@ -5,6 +5,7 @@ export const atomWithAsyncStorage = <T>(_key: string, initialValue: T) => {
   const key = `atomWithAsyncStorage:${_key}`;
 
   const baseAtom = atom<T>(initialValue);
+  const batchUpdates = new Set<[string, string]>();
 
   baseAtom.onMount = (setValue) => {
     (async () => {
@@ -19,9 +20,16 @@ export const atomWithAsyncStorage = <T>(_key: string, initialValue: T) => {
     (get, set, update: T | ((prev: T) => T)) => {
       const nextValue = typeof update === 'function' ? (update as Function)(get(baseAtom)) : update;
       set(baseAtom, nextValue);
-      AsyncStorage.setItem(key, JSON.stringify(nextValue));
+      batchUpdates.add([key, JSON.stringify(nextValue)]);
     }
   );
+
+  setInterval(async () => {
+    if (batchUpdates.size === 0) return;
+    const updates = Array.from(batchUpdates);
+    batchUpdates.clear();
+    await AsyncStorage.multiSet(updates);
+  }, 5_000);
 
   return derivedAtom;
 };
