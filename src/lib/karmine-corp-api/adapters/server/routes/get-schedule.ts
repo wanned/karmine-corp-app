f89156a4dd1e0083985a1c7ab56c1d27fdb17efa
Arthur Fontaine <0arthur.fontaine@gmail.com>
2024-03-24 23:50:44 +0100

    Provide only once the main layer in server

--- src/lib/karmine-corp-api/adapters/server/routes/get-schedule.ts
+++ src/lib/karmine-corp-api/adapters/server/routes/get-schedule.ts
@@ -1,83 +1,74 @@
 import { HttpServer } from '@effect/platform';
-import { Chunk, Effect, Layer, Stream } from 'effect';
+import { Chunk, Context, Effect, Layer, Stream } from 'effect';
 
 import { getSchedule } from '~/lib/karmine-corp-api/application/use-cases/get-schedule/get-schedule';
-import { createBetterSqlite3Impl } from '~/lib/karmine-corp-api/infrastructure/services/database/better-sqlite3-impl';
 import { DatabaseService } from '~/lib/karmine-corp-api/infrastructure/services/database/database-service';
 import { EnvService } from '~/lib/karmine-corp-api/infrastructure/services/env/env-service';
-import { FetchServiceImpl } from '~/lib/karmine-corp-api/infrastructure/services/fetch/fetch-service-impl';
-import { KarmineApiServiceImpl } from '~/lib/karmine-corp-api/infrastructure/services/karmine-api/karmine-api-service-impl';
-import { LeagueOfLegendsApiServiceImpl } from '~/lib/karmine-corp-api/infrastructure/services/league-of-legends-api/league-of-legends-api-service-impl';
-import { OctaneApiServiceImpl } from '~/lib/karmine-corp-api/infrastructure/services/octane-api/octane-api-service-impl';
-import { StrafeApiServiceImpl } from '~/lib/karmine-corp-api/infrastructure/services/strafe-api/strafe-api-service-impl';
+import { FetchService } from '~/lib/karmine-corp-api/infrastructure/services/fetch/fetch-service';
+import { KarmineApiService } from '~/lib/karmine-corp-api/infrastructure/services/karmine-api/karmine-api-service';
+import { LeagueOfLegendsApiService } from '~/lib/karmine-corp-api/infrastructure/services/league-of-legends-api/league-of-legends-api-service';
+import { OctaneApiService } from '~/lib/karmine-corp-api/infrastructure/services/octane-api/octane-api-service';
+import { StrafeApiService } from '~/lib/karmine-corp-api/infrastructure/services/strafe-api/strafe-api-service';
 
 export const GetScheduleRoute = () =>
   HttpServer.router.get(
     '/schedule',
-    getMainLayer().pipe((mainLayer) =>
-      Effect.provide(
-        Effect.Do.pipe(
-          Effect.flatMap(() => DatabaseService.pipe(Effect.flatMap((_) => _.initializeTables()))),
-          Effect.map(() =>
-            HttpServer.response.setHeaders({
-              'Content-Type': 'application/json',
+    Effect.Do.pipe(
+      Effect.flatMap(() =>
+        Effect.serviceFunctionEffect(DatabaseService, (_) => _.initializeTables)()
+      ),
+      Effect.map(() =>
+        Stream.concatAll(
+          Chunk.make(
+            Stream.make('[\n'),
+            getSchedule().pipe(
+              Stream.filter(
+                (schedule): schedule is Exclude<typeof schedule, undefined | void> =>
+                  schedule !== undefined
+              ),
+              Stream.zipWithIndex,
+              Stream.map(
+                ([schedule, index]) => `${index === 0 ? '' : ',\n'}${JSON.stringify(schedule)}`
+              )
+            ),
+            Stream.make('\n]')
+          )
+        ).pipe(Stream.map((schedule) => new TextEncoder().encode(schedule)))
+      ),
+      Effect.flatMap((schedule) =>
+        getLayers([
+          LeagueOfLegendsApiService,
+          OctaneApiService,
+          KarmineApiService,
+          StrafeApiService,
+          FetchService,
+          DatabaseService,
+          EnvService,
+        ]).pipe(
+          Effect.map((layers) => schedule.pipe(Stream.provideLayer(layers))),
+          Effect.map((schedule) =>
+            HttpServer.response.stream(schedule, {
+              contentType: 'application/json',
             })
-          ),
-          Effect.map(() =>
-            Stream.provideLayer(
-              Stream.concatAll(
-                Chunk.fromIterable([
-                  Stream.make('[\n'),
-                  getSchedule().pipe(
-                    Stream.filter(
-                      (schedule): schedule is Exclude<typeof schedule, undefined | void> =>
-                        schedule !== undefined
-                    ),
-                    Stream.zipWithIndex,
-                    Stream.map(
-                      ([schedule, index]) =>
-                        `${index === 0 ? '' : ',\n'}${JSON.stringify(schedule)}`
-                    )
-                  ),
-                  Stream.make('\n]'),
-                ])
-              ).pipe(Stream.map((schedule) => new TextEncoder().encode(schedule))),
-              mainLayer
-            )
-          ),
-          Effect.map((schedule) => HttpServer.response.stream(schedule))
-        ),
-        mainLayer
+          )
+        )
       )
     )
   );
 
-const getMainLayer = () =>
-  Layer.mergeAll(
-    LeagueOfLegendsApiServiceImpl,
-    OctaneApiServiceImpl,
-    KarmineApiServiceImpl,
-    StrafeApiServiceImpl,
-    FetchServiceImpl,
-    createBetterSqlite3Impl(),
-    Layer.succeed(
-      EnvService,
-      EnvService.of({
-        getEnv: () =>
-          Effect.succeed({
-            OCTANE_API_URL: 'https://zsr.octane.gg',
-            LOL_ESPORT_API_URL: 'https://esports-api.lolesports.com/persisted/gw',
-            LOL_FEED_API_URL: 'https://feed.lolesports.com/livestats/v1',
-            LOL_DATA_DRAGON_API_URL: 'https://ddragon.leagueoflegends.com',
-            LOL_API_KEY: '0TvQnueqKa5mxJntVWt0w4LpLfEkrV1Ta8rQBb9Z',
-            STRAFE_API_URL: 'https://flask-api.strafe.com',
-            STRAFE_API_KEY:
-              'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJ1c2VyX2lkIjoxMDAwLCJpYXQiOjE2MTE2NTM0MzcuMzMzMDU5fQ.n9StQPQdpNIx3E4FKFntFuzKWolstKJRd-T4LwXmfmo',
-            KARMINE_API_URL: 'https://api2.kametotv.fr/karmine',
-            LIQUIPEDIA_PARSE_API_URL: 'https://liquipedia.net/<GAME>/api.php',
-            LIQUIPEDIA_PARSE_URL_GAME_REPLACER: '<GAME>',
-            YOUTUBE_API_URL: 'https://www.youtube.com',
-          }),
-      })
-    )
-  );
+const getLayers = <Layers extends Context.Tag<any, any>>(
+  _layers: Layers[]
+): Effect.Effect<
+  Layer.Layer<Layers extends { prototype: any } ? Layers['prototype'] : Layers, never, never>,
+  never,
+  Layers extends { prototype: any } ? Layers['prototype'] : Layers
+> => Effect.succeed(Layer.empty) as any; // NOTE: It works. I suspect that the layers are automatically passed from the parent, and that functions like Stream.provideLayer are only for type-checking.
+
+// Effect.Do.pipe(
+//   Effect.flatMap(() =>
+//     Effect.forEach(layers, (layer) =>
+//       layer.pipe(Effect.map((_) => Layer.succeed(layer, layer.of(_))))
+//     )
+//   ),
+//   Effect.map((layers) => Layer.mergeAll(...(layers as any)))
+// ) as any;
