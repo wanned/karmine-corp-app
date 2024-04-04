c24d1260ab33afc8b2a550b6e11d09448b024aae
Arthur Fontaine <0arthur.fontaine@gmail.com>
2024-04-04 12:48:46 +0200

    Refactoring getRocketLeagueSchedule service

---
+++ src/lib/karmine-corp-api/application/services/get-rocket-league-schedule/convert-to-core-match/convert-to-core-match.ts
@@ -0,0 +1,90 @@
+import { Effect } from 'effect';
+
+import { CoreData } from '../../../types/core-data';
+
+import { OctaneApi } from '~/lib/karmine-corp-api/infrastructure/services/octane-api/octane-api';
+import { OctaneApiService } from '~/lib/karmine-corp-api/infrastructure/services/octane-api/octane-api-service';
+
+type RocketLeagueMatch = OctaneApi.GetMatches['matches'][number];
+
+export function convertToCoreMatch(match: RocketLeagueMatch) {
+  return Effect.all({
+    id: Effect.succeed(`rl:${match._id}`),
+    date: Effect.succeed(match.date),
+    matchDetails: Effect.all({
+      competitionName: Effect.succeed(CoreData.CompetitionName.RocketLeague as const),
+      bo: Effect.succeed(match.format.length),
+      games: getGames(match),
+      players: Effect.all({
+        home: Effect.forEach(match.blue.players, (player) =>
+          Effect.succeed({ name: player.player.tag })
+        ),
+        away: Effect.forEach(match.orange.players, (player) =>
+          Effect.succeed({ name: player.player.tag })
+        ),
+      }),
+    }),
+    status: getStatus(match),
+    teams: Effect.all([getTeam(match, 'blue'), getTeam(match, 'orange')]),
+    streamLink: Effect.succeed('kamet0'), // TODO
+  }) satisfies Effect.Effect<CoreData.RocketLeagueMatch, any, any>;
+}
+
+function getGames(match: RocketLeagueMatch) {
+  return Effect.Do.pipe(
+    Effect.flatMap(() =>
+      Effect.serviceMembers(OctaneApiService).functions.getMatchGames({ matchId: match._id })
+    ),
+    Effect.map((response) =>
+      response.games.map((game) =>
+        game.blue.team.stats === undefined || game.orange.team.stats === undefined ?
+          undefined
+        : {
+            teams: {
+              home: {
+                goals: game.blue.team.stats.core.goals,
+                stops: game.blue.team.stats.core.saves,
+                totalPoints: game.blue.team.stats.core.score,
+              },
+              away: {
+                goals: game.orange.team.stats.core.goals,
+                stops: game.orange.team.stats.core.saves,
+                totalPoints: game.orange.team.stats.core.score,
+              },
+            },
+          }
+      )
+    )
+  ) satisfies Effect.Effect<CoreData.RocketLeagueMatch['matchDetails']['games'], any, any>;
+}
+
+function getStatus(match: RocketLeagueMatch) {
+  return Effect.Do.pipe(
+    Effect.map(() =>
+      match.blue.score + match.orange.score === 0 ? 'upcoming'
+      : [match.blue.score, match.orange.score].includes(Math.ceil(match.format.length / 2)) ?
+        'finished'
+      : 'live'
+    )
+  ) satisfies Effect.Effect<CoreData.RocketLeagueMatch['status'], any, any>;
+}
+
+function getTeam(match: RocketLeagueMatch, team: 'blue' | 'orange') {
+  return Effect.Do.pipe(
+    Effect.flatMap(() => getStatus(match)),
+    Effect.map((status) => ({
+      name: match[team].team.team.name,
+      logoUrl:
+        match[team].team.team.image ??
+        'https://medias.kametotv.fr/karmine/teams_logo/NO_TEAM_RL.png',
+      score:
+        status === 'upcoming' ? undefined : (
+          {
+            score: match[team].score,
+            isWinner: match[team].winner,
+            scoreType: 'gameWins',
+          }
+        ),
+    }))
+  ) satisfies Effect.Effect<CoreData.RocketLeagueMatch['teams'][0], any, any>;
+}
---
+++ src/lib/karmine-corp-api/application/services/get-rocket-league-schedule/paginate-karmine-corp-matches.ts
@@ -0,0 +1,75 @@
+import { Chunk, Effect, Stream, Option } from 'effect';
+
+import { PaginateChunkEffectPaginatorFormatter } from '../../types/effect-utils';
+import { GetScheduleParamsState } from '../../use-cases/get-schedule/get-schedule-params-state';
+
+import { OctaneApi } from '~/lib/karmine-corp-api/infrastructure/services/octane-api/octane-api';
+import { OctaneApiService } from '~/lib/karmine-corp-api/infrastructure/services/octane-api/octane-api-service';
+
+type RocketLeagueMatch = OctaneApi.GetMatches['matches'][number];
+
+const KARMINE_CORP_OCTANE_TEAM_ID = '60fbc5b887f814e9fbffdcbd';
+
+export function paginateKarmineCorpMatches() {
+  return Stream.paginateChunkEffect(undefined as OctaneApi.GetMatches | undefined, (lastResponse) =>
+    Effect.scoped(getNextMatchesGetter()(lastResponse))
+  );
+}
+
+function getNextMatchesGetter() {
+  return function getNextMatches(lastResponse: OctaneApi.GetMatches | undefined) {
+    return Effect.Do.pipe(
+      Effect.flatMap(() =>
+        Effect.serviceMembers(OctaneApiService).functions.getMatches({
+          teamId: KARMINE_CORP_OCTANE_TEAM_ID,
+          page: lastResponse ? lastResponse.page + 1 : undefined,
+          sort: 'date:desc',
+        })
+      ),
+      Effect.flatMap(getResultForPaginatorFormatter())
+    );
+  };
+}
+
+function getResultForPaginatorFormatter(): PaginateChunkEffectPaginatorFormatter<
+  OctaneApi.GetMatches,
+  RocketLeagueMatch,
+  never,
+  GetScheduleParamsState
+> {
+  return function formatResultForPaginator(schedule) {
+    return Effect.gen(function* formatResultForPaginatorGenerator(_) {
+      let karmineMatchesChunk = Chunk.fromIterable(schedule.matches);
+      let shouldContinuePagination = true;
+
+      // Filter by date
+      const dateRange = yield* _(Effect.serviceConstants(GetScheduleParamsState).dateRange);
+      if (dateRange !== undefined) {
+        let enteredInDateRange = false;
+        karmineMatchesChunk = Chunk.filter(karmineMatchesChunk, (event) => {
+          if (
+            (dateRange.start === undefined || event.date >= dateRange.start) &&
+            (dateRange.end === undefined || event.date <= dateRange.end)
+          ) {
+            enteredInDateRange = true;
+            return true;
+          }
+          return false;
+        });
+
+        if (!enteredInDateRange) {
+          shouldContinuePagination = true;
+        }
+      }
+
+      shouldContinuePagination = shouldContinuePagination && karmineMatchesChunk.length === 0;
+      shouldContinuePagination = shouldContinuePagination && schedule.pageSize < schedule.perPage;
+
+      if (!shouldContinuePagination) {
+        return [karmineMatchesChunk, Option.none()];
+      }
+
+      return [karmineMatchesChunk, Option.some(schedule)];
+    });
+  };
+}
