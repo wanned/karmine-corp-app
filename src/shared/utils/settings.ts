32aa136a33943c357ecd29304d34948467203eb5
Arthur Fontaine <0arthur.fontaine@gmail.com>
2024-04-04 16:31:33 +0200

    Use device language as default

--- src/shared/utils/settings.ts
+++ src/shared/utils/settings.ts
@@ -1,13 +1,16 @@
 import AsyncStorage from '@react-native-async-storage/async-storage';
 import defu from 'defu';
+import * as Localization from 'expo-localization';
 
 import { Settings } from '../types/Settings';
+
+import { Language } from '~/translations/Translations';
 
 const SETTINGS_ASYNC_STORAGE_KEY = 'settings';
 
 const defaultSettings: Settings = {
   showResults: true,
-  language: 'en',
+  language: (Localization.getLocales()[0].languageCode as Language) ?? 'en',
   notifications: {
     LeagueOfLegendsLEC: true,
     LeagueOfLegendsLFL: true,
