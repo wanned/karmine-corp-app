53735d851ae737a5a53ab455c4660a0c330b6e91
Arthur Fontaine <0arthur.fontaine@gmail.com>
2024-04-07 17:49:17 +0200

    Fix bad schedule LoL API parsing

--- src/lib/karmine-corp-api/infrastructure/services/league-of-legends-api/schemas/get-schedule-by-league-ids-schema.ts
+++ src/lib/karmine-corp-api/infrastructure/services/league-of-legends-api/schemas/get-schedule-by-league-ids-schema.ts
@@ -21,27 +21,35 @@ export const getScheduleByLeagueIdsSchema = v.object({
         older: v.string().nullable().optional(),
         newer: v.string().nullable().optional(),
       }),
-      events: v.array(
-        v.union(
-          v.object({
-            type: v.literal('match'),
-            startTime: vDateString,
-            state: v.union(v.literal('unstarted'), v.literal('inProgress'), v.literal('completed')),
-            league: v.object({
-              slug: v.string(),
-            }),
-            match: v.object({
-              id: v.string(),
-              teams: v.tuple([teamSchema, teamSchema]),
-              strategy: v.object({
-                type: v.literal('bestOf'),
-                count: v.number(),
+      events: v
+        .array(
+          v.union(
+            v.object({
+              type: v.literal('match'),
+              startTime: vDateString,
+              state: v.union(
+                v.literal('unstarted'),
+                v.literal('inProgress'),
+                v.literal('completed')
+              ),
+              league: v.object({
+                slug: v.string(),
+              }),
+              match: v.object({
+                id: v.string(),
+                teams: v.tuple([teamSchema, teamSchema]),
+                strategy: v.object({
+                  type: v.literal('bestOf'),
+                  count: v.number(),
+                }),
               }),
             }),
-          }),
-          v.never()
+            v.unknown().chain(() => v.ok(undefined))
+          )
         )
-      ),
+        .chain((data) =>
+          v.ok(data.filter((d): d is NonNullable<typeof d> => d !== null && d !== undefined))
+        ),
     }),
   }),
 });
