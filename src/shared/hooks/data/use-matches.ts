97040166a5004822a539a341487d6cf73af60998
Arthur Fontaine <0arthur.fontaine@gmail.com>
2024-05-19 20:04:31 +0200

    Add matches in the useMatches exposed hook

--- src/shared/hooks/data/use-matches.ts
+++ src/shared/hooks/data/use-matches.ts
@@ -1,1 +1,16 @@
-export { useMatches } from '~/lib/karmine-corp-api/adapters/react-native/use-matches';
+import { useStore } from '@nanostores/react';
+
+import {
+  useMatches as _useMatches,
+  matchesAtom,
+} from '~/lib/karmine-corp-api/adapters/react-native/use-matches';
+
+export const useMatches = () => {
+  const { matchesFetchingStatus } = _useMatches();
+  const matches = useStore(matchesAtom);
+
+  return {
+    matchesFetchingStatus,
+    matches,
+  };
+};
